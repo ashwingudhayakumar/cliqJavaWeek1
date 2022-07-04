aea4f4005e07e6429565e83b9b2aeeb3694d8a96
ashwingudhayakumar <88505120+ashwingudhayakumar@users.noreply.github.com>
2022-07-04 12:34:12 +0530

    after refactoring

--- program4.java
+++ program4.java
@@ -1,27 +1,13 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
 import java.util.ArrayList;
 class HelloWorld {
-    public static void main(String[] args) {
-         Scanner sc= new Scanner(System.in);
-         System.out.println("Enter the size of the array");
-         int size=sc.nextInt();
-         int arr[]=new int[size];
-         System.out.println("Enter elements");
-         for(int i=0;i<arr.length;i++){
-             arr[i]=sc.nextInt();
-         }
-         //printing the array element
-         for(int i=0;i<arr.length;i++){
-             System.out.println(arr[i]);
-         }
-         //this is for insertion
-         System.out.println("Insert element and index :[inbetween 0 to size]");
+    static Scanner sc= new Scanner(System.in);
+    public static int[] insertion( int arr[]){
+        System.out.println("Insert element and index :[inbetween 0 to size]");
          int elementToInsert=sc.nextInt();
          int positionToInsert=sc.nextInt();
-         if(positionToInsert>=0 && positionToInsert<=size){
-          int tempArr[]=new int[size+1];
+         if(positionToInsert>=0 && positionToInsert<=arr.length){
+          int tempArr[]=new int[arr.length+1];
           int i=0;
           while(i<positionToInsert){
               tempArr[i]=arr[i];
@@ -41,8 +27,11 @@ class HelloWorld {
           for(int i=0;i<arr.length;i++){
              System.out.println(arr[i]);
          }
-         //this is for removal
-         System.out.println("enter the index to be removed :[inbetween 0 to size-1]");
+         return arr;
+    }
+    
+     public static int[] removal( int arr[]){
+        System.out.println("enter the index to be removed :[inbetween 0 to size-1]");
          int positionToRemove=sc.nextInt();
          if(positionToRemove>=0 && positionToRemove<arr.length){
              int tempArr[]=new int[arr.length-1];
@@ -65,6 +54,10 @@ class HelloWorld {
          for(int i=0;i<arr.length;i++){
              System.out.println(arr[i]);
          }
+         return arr;
+    }
+    
+    public static void displayValue(int arr[]){
          //get values
          System.out.println("Enter the index to be displayed");
          int displayIndex=sc.nextInt();
@@ -73,9 +66,10 @@ class HelloWorld {
          else{
                System.out.println("Invalid index"); 
          }
-         //sort
-         
-         for(int i=0;i<arr.length-1;i++){
+    }
+    
+    public static void sort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
              if(arr[i]>arr[i+1]){
                  int temp=arr[i];
                  arr[i]=arr[i+1];
@@ -88,12 +82,17 @@ class HelloWorld {
          for(int i=0;i<arr.length;i++){
              System.out.println(arr[i]);
          }
-         
-         //mininum and maximum
+    }
+    
+    public static void minMax(int arr[]){
+        sort(arr);
+        //mininum and maximum
          System.out.println("min "+arr[0]);
          System.out.println("max "+arr[arr.length-1]);
-         
-         //duplicate removal
+    }
+    
+    public static int[] duplicateRemoval(int arr[]){
+                 //duplicate removal
          ArrayList al=new ArrayList();
          for(int i=0;i<arr.length-1;i++){
              if(arr[i]!=arr[i+1]){
@@ -102,7 +101,72 @@ class HelloWorld {
          }
          al.add(arr[arr.length-1]);
          
+         int arrAfterDubRemoval[]=new int [al.size()];
+         for(int itr=0;itr<al.size();itr++){
+             arrAfterDubRemoval[itr]=(int)al.get(itr);
+             System.out.println(arrAfterDubRemoval[itr]);
+         }
+         
          //after removing dublicate
          System.out.println(al);
+         return arrAfterDubRemoval;
+    }
+    
+    
+    public static void main(String[] args) {
+         
+         System.out.println("Enter the size of the array");
+         int size=sc.nextInt();
+         int arr[]=new int[size];
+         System.out.println("Enter elements");
+         for(int i=0;i<arr.length;i++){
+             arr[i]=sc.nextInt();
+         }
+         //printing the array element
+         for(int i=0;i<arr.length;i++){
+             System.out.println(arr[i]);
+         }
+         boolean continued=true;
+        while(continued){
+            System.out.println("For Insertion press i and enter, For removal press r and enter,For certain index to be printed press c and enter, For sort press s and enter,For displaying min and max value press m and enter,For dublicate removal press d and enter");
+             char check=sc.next().charAt(0);
+            switch(check){
+                case 'i':
+         arr=insertion(arr);
+         System.out.println("for continue type true to break type false");
+         continued=sc.nextBoolean();
+         break;
+         case 'r':
+         arr=removal(arr);
+           System.out.println("for continue type true to break type false");
+         continued=sc.nextBoolean();
+          break;
+         case 'c':
+         displayValue(arr);
+           System.out.println("for continue type true to break type false");
+         continued=sc.nextBoolean();
+          break;
+         case 's':
+         sort(arr);
+           System.out.println("for continue type true to break type false");
+         continued=sc.nextBoolean();
+          break;
+         case 'm':
+         minMax(arr);
+           System.out.println("for continue type true to break type false");
+         continued=sc.nextBoolean();
+          break;
+         case 'd':
+            arr= duplicateRemoval(arr);
+               System.out.println("for continue type true to break type false");
+         continued=sc.nextBoolean();
+         
+          break;
+            }
+        }
+         
+         
+         
+
     }
 }
